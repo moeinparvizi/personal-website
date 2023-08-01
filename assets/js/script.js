a0c3362d0078ff31c19a9380228ff83df24671ae
moeinparvizi <giventulip@gmail.com>
2023-08-02 01:30:04 +0330

    fix sum bugs and create title text sections and create glass section and fix responsive bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function displayMenu(sub,menuH){
-    sub.style.transition = "1s";
+        sub.style.transition = "1s";
     if (sub.style.opacity == "0"){
         sub.style.opacity = "1";
         sub.style.visibility = 'visible'
