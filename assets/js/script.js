b54625124b737b4327a55c25a9f56bcd73af058c
moeinparvizi <giventulip@gmail.com>
2023-08-11 00:11:00 +0330

    update js file for scroll animation skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,58 @@
-function displayMenu(sub,menuH){
-        sub.style.transition = "1s";
-    if (sub.style.opacity == "0"){
-        sub.style.opacity = "1";
-        sub.style.visibility = 'visible'
-        menuH.classList.add('clickMenuEffect');
+function displayMenu(sub, menuH) {
+  sub.style.transition = "1s";
+  if (sub.style.opacity == "0") {
+    sub.style.opacity = "1";
+    sub.style.visibility = 'visible'
+    menuH.classList.add('clickMenuEffect');
 
-    }
-    else{
-        sub.style.opacity = "0";
-        sub.style.visibility = 'hidden'
-        menuH.classList.remove('clickMenuEffect');
-    }
+  }
+  else {
+    sub.style.opacity = "0";
+    sub.style.visibility = 'hidden'
+    menuH.classList.remove('clickMenuEffect');
+  }
 }
 
 socials.style.top = "0px";
 socials.style.opacity = "0";
-function socialUp(){
-    let socials = document.getElementById('socials');
-    socials.style.transition = ".4s";
-    if (socials.style.top == "0px"){
-        socials.style.top = "-55px";
-        socials.style.opacity = "1";
-    }
-    else{
-        socials.style.top = "0px";
-        socials.style.opacity = "0";
-    }
+function socialUp() {
+  let socials = document.getElementById('socials');
+  socials.style.transition = ".4s";
+  if (socials.style.top == "0px") {
+    socials.style.top = "-55px";
+    socials.style.opacity = "1";
+  }
+  else {
+    socials.style.top = "0px";
+    socials.style.opacity = "0";
+  }
 }
+
+// scroll animation
+const boxes = document.querySelectorAll(".box")
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+
+  const triggerBottom = window.innerHeight / 5 * 4
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
+
+// scroll skills animation
+window.addEventListener('scroll', function(event) {
+  const scroll = this.scrollY
+  console.log(scroll)
+  if (scroll > 1300) {
+    this.document.getElementById('skills').classList.add('showSkills');
+  }
+  else {
+    this.document.getElementById('skills').classList.remove('showSkills')
+  }
+})
